097d463a5420e6160a52a0bce31dfb4ffa38fa95
zhangnan <nan.zhang@qingteng.cn>
2018-12-05 20:52:31 +0800

    [fix]保证编译正确

--- helper/src/main/java/lombok/javac/handlers/HandleHelloWorld.java
+++ helper/src/main/java/lombok/javac/handlers/HandleHelloWorld.java
@@ -16,49 +16,53 @@ import java.lang.reflect.Modifier;
 
 import static lombok.core.handlers.HandlerUtil.handleFlagUsage;
 
-@ProviderFor(JavacAnnotationHandler.class)
-public class HandleHelloWorld extends JavacAnnotationHandler<HelloWorld> {
-    @Override
-    public void handle(AnnotationValues<HelloWorld> annotation, JCTree.JCAnnotation ast, JavacNode annotationNode) {
-        handleFlagUsage(annotationNode, ConfigurationKeys.GETTER_FLAG_USAGE, "@HelloWorld");
-        JavacNode typeNode = annotationNode.up();
+public class HandleHelloWorld {
 
-        JCTree.JCMethodDecl helloWorldMethod = createHelloWorld(typeNode);
-
-        JavacHandlerUtil.injectMethod(typeNode, helloWorldMethod);
-    }
-
-    private JCTree.JCMethodDecl createHelloWorld(JavacNode type) {
-        JavacTreeMaker treeMaker = type.getTreeMaker();
-
-        JCTree.JCModifiers modifiers = treeMaker.Modifiers(Modifier.PUBLIC);
-        List<JCTree.JCTypeParameter> methodGenericTypes = List.<JCTree.JCTypeParameter>nil();
-        JCTree.JCExpression methodType = treeMaker.TypeIdent(Javac.CTC_VOID);
-        Name methodName = type.toName("helloWorld");
-        List<JCTree.JCVariableDecl> methodParameters = List.<JCTree.JCVariableDecl>nil();
-        List<JCTree.JCExpression> methodThrows = List.<JCTree.JCExpression>nil();
-
-        JCTree.JCExpression printlnMethod =
-                JavacHandlerUtil.chainDots(type, "System", "out", "println");
-        List<JCTree.JCExpression> printlnArgs = List.<JCTree.JCExpression>of(treeMaker.Literal("hello world"));
-        JCTree.JCMethodInvocation printlnInvocation =
-                treeMaker.Apply(List.<JCTree.JCExpression>nil(), printlnMethod, printlnArgs);
-        JCTree.JCBlock methodBody =
-                treeMaker.Block(0, List.<JCTree.JCStatement>of(treeMaker.Exec(printlnInvocation)));
-
-        JCTree.JCExpression defaultValue = null;
-
-        return treeMaker.MethodDef(
-                modifiers,
-                methodName,
-                methodType,
-                methodGenericTypes,
-                methodParameters,
-                methodThrows,
-                methodBody,
-                defaultValue
-        );
-    }
 }
+
+//@ProviderFor(JavacAnnotationHandler.class)
+//public class HandleHelloWorld extends JavacAnnotationHandler<HelloWorld> {
+//    @Override
+//    public void handle(AnnotationValues<HelloWorld> annotation, JCTree.JCAnnotation ast, JavacNode annotationNode) {
+//        handleFlagUsage(annotationNode, ConfigurationKeys.GETTER_FLAG_USAGE, "@HelloWorld");
+//        JavacNode typeNode = annotationNode.up();
+//
+//        JCTree.JCMethodDecl helloWorldMethod = createHelloWorld(typeNode);
+//
+//        JavacHandlerUtil.injectMethod(typeNode, helloWorldMethod);
+//    }
+//
+//    private JCTree.JCMethodDecl createHelloWorld(JavacNode type) {
+//        JavacTreeMaker treeMaker = type.getTreeMaker();
+//
+//        JCTree.JCModifiers modifiers = treeMaker.Modifiers(Modifier.PUBLIC);
+//        List<JCTree.JCTypeParameter> methodGenericTypes = List.<JCTree.JCTypeParameter>nil();
+//        JCTree.JCExpression methodType = treeMaker.TypeIdent(Javac.CTC_VOID);
+//        Name methodName = type.toName("helloWorld");
+//        List<JCTree.JCVariableDecl> methodParameters = List.<JCTree.JCVariableDecl>nil();
+//        List<JCTree.JCExpression> methodThrows = List.<JCTree.JCExpression>nil();
+//
+//        JCTree.JCExpression printlnMethod =
+//                JavacHandlerUtil.chainDots(type, "System", "out", "println");
+//        List<JCTree.JCExpression> printlnArgs = List.<JCTree.JCExpression>of(treeMaker.Literal("hello world"));
+//        JCTree.JCMethodInvocation printlnInvocation =
+//                treeMaker.Apply(List.<JCTree.JCExpression>nil(), printlnMethod, printlnArgs);
+//        JCTree.JCBlock methodBody =
+//                treeMaker.Block(0, List.<JCTree.JCStatement>of(treeMaker.Exec(printlnInvocation)));
+//
+//        JCTree.JCExpression defaultValue = null;
+//
+//        return treeMaker.MethodDef(
+//                modifiers,
+//                methodName,
+//                methodType,
+//                methodGenericTypes,
+//                methodParameters,
+//                methodThrows,
+//                methodBody,
+//                defaultValue
+//        );
+//    }
+//}
 
 
